7d7bba059d76a1986506395f4d1a969180a3b76b
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-12-31 23:45:20 +0900

    Use sqlite

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,13 @@ extern crate diesel;
 extern crate dotenv;
 
 use diesel::prelude::*;
-use diesel::pg::PgConnection;
+use diesel::sqlite::SqliteConnection;
 use dotenv::dotenv;
 use std::env;
 
-pub fn establish_connection() -> PgConnection {
+pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+    SqliteConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url))
 }
