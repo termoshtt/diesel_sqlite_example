6010846f32f7b32b88f0a2ec4cff521c2fd26a5f
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-01-01 12:14:23 +0900

    Add create post

--- src/lib.rs
+++ src/lib.rs
@@ -13,13 +13,27 @@ include!("lib.in.rs");
 #[cfg(feature = "with-syntex")]
 include!(concat!(env!("OUT_DIR"), "/lib.rs"));
 
+use std::env;
 use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 use dotenv::dotenv;
-use std::env;
+
+use self::schema::posts;
+use self::models::{Post, NewPost};
 
 pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     SqliteConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
+
+pub fn create_post(conn: &SqliteConnection, title: &str, body: &str) -> Post {
+    let new_post = NewPost {
+        title: title,
+        body: body,
+    };
+    diesel::insert(&new_post)
+        .into(posts::table)
+        .get_result(conn)
+        .expect("Error saving new post")
 }
